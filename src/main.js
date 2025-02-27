dfe6f625a5b6c0123bab5a2cce5c16786532bf66
Marcos Pirela <marcosj465@gmail.com>
2025-02-27 17:06:59 -0400

    Creating the navbar

--- src/main.js
+++ src/main.js
@@ -2,15 +2,35 @@ import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
-
+import logo from "../public/logos/Recurso 13.svg";
+import facebook from "../public/logos/facebook.svg";
+import instagram from "../public/logos/instagram.svg";
 document.querySelector("#app").innerHTML = `
-  <div>
-<div class="hero">
-<p class="padding-0">MÁS PUBLICIDAD! SUBSCRÍBETE A NUESTRA NEWSLETTER</p>
-<a href="#">Clic aquí</a>
-  <a href="#" >Clic aquí</a>
+<div>
+      <div class="hero">
+        <p class="margin-0 w-5-6 text-xs"> <span class="font-100"> MÁS PUBLICIDAD! </span> SUBSCRÍBETE A NUESTRA NEWSLETTER</p>
+        <a class="w-1-6 text-xs none" href="#">Clic aquí</a>
+      </div>
+      <div class="navbar">
+        <a href="#">  <img class="logo" src="${logo}" alt="logo" srcset=""></a>
+        <div>
+            <ul>
+              <li><a href="Nosotros.asp">Nosotros</a></li>
+              <li><a href="Servicios.asp">Servicios</a></li>
+              <li><a href="Proyectos.asp">Proyectos</a></li>
+              <li><a href="Contacto.asp">Contacto</a></li>
+            </ul>
+        </div>
+        <div>
+        <a href="http://www.instagram.com">
+          <img  src="${instagram}" alt="" srcset="">
+        </a>
+        <a href="http://www.facebook.com">
+          <img  src="${facebook}" alt="" srcset="">
+        </a>
+        </div>
+      </div>
 </div>
-  </div>
 `;
 
 setupCounter(document.querySelector("#counter"));
